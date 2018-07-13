4f803cca6d1e45e35227e1411d1129a104ee5ca1
Shane Hobson <shanehobson1@gmail.com>
2018-07-12 21:06:28 -0400

    Style updates to NavBar component

--- loaders-app/src/components/HomePage.js
+++ loaders-app/src/components/HomePage.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import NavBar from './NavBar';
 import SpinnerList from './SpinnerList';
+import '../styles/NavBar.css';
 import '../styles/Spinner.css';
 
 const spinners = [{prop: 'exampleSpinnerProp'}, 
   {prop: 'exampleSpinnerProp'}, 
   {prop: 'exampleSpinnerProp'}];
 
-export const HomePage = () => (
-
+export default () => (
   <div>
     <NavBar />
     <SpinnerList spinners={spinners}/>
   </div>
 );
 
-export default HomePage;
 
 
