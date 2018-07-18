f83cc6728686521a520c606898ead3be30990434
Shane Hobson <shanehobson1@gmail.com>
2018-07-18 15:49:00 -0400

    Added Header Component

--- loaders-app/src/components/HomePage.js
+++ loaders-app/src/components/HomePage.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import NavBar from './NavBar';
+import Header from './Header';
 import SpinnerList from './SpinnerList';
 import '../styles/Spinner.css';
+import '../styles/Header.css';
 
 const spinners = [{prop: 'exampleSpinnerProp'}, 
   {prop: 'exampleSpinnerProp'}, 
   {prop: 'exampleSpinnerProp'}];
 
-export const HomePage = () => (
-
+const HomePage = () => (
   <div>
     <NavBar />
+    <Header />
     <SpinnerList spinners={spinners}/>
   </div>
 );
