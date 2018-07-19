8072c14d1233a564e306e503bee48610d61781ec
Shane Hobson <shanehobson1@gmail.com>
2018-07-18 22:35:47 -0400

    Resolved conflict in HomePage.js file

--- loaders-app/src/components/HomePage.js
+++ loaders-app/src/components/HomePage.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import NavBar from './NavBar';
+import Header from './Header';
 import SearchBar from './SearchBar';
 import SpinnerList from './SpinnerList';
 import '../styles/Spinner.css';
 import '../styles/LayoutStyles.css';
+import '../styles/Header.css';
 
 const spinners = [{prop: 'exampleSpinnerProp'}, 
   {prop: 'exampleSpinnerProp'}, 
